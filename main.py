4b9a2cbfe1c37c30debff930f376869384c522e0
daviddserb <83017887+daviddserb@users.noreply.github.com>
2021-04-22 21:58:34 +0300

    2048

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
 import random
-marieneeeeeeeeee te iau in ...
-haide ma sa ne o dam
-v-o iau la amandoi deodata
-ohohoho nice!
-vavavavavavavavav
-dsadsadsadsadasdsa
-dsadsadasfas21312
-EZ BOAIiiiiii
-412421
+import colors
