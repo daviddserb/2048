d869732c9e03f862a9c14006286e27e592bb16ff
daviddserb <83017887+daviddserb@users.noreply.github.com>
2021-04-28 13:53:44 +0300

    2048

--- main.py
+++ main.py
@@ -3,14 +3,24 @@ import random
 
 N = 4
 
-grid = np.zeros((N, N))  # un grid de N, N (pentru ca ii un patrat de N linii si N coloane, N fiind 4)
 
-poss = list((zip(*np.where(grid == 0))))  # zip le grupeaza 2 cate 2, adica inainte lista ta era [(0, 0, 0, 0, 1, 1, etc...)] acum este [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), etc...]
+class Py2048Logic:  # fac doar logica acum
+    def __init__(self):
+        self.grid = np.zeros((N, N))  # initializam grila de (N, N), pentru ca este un patrat de N linii si N coloane)
 
-for pos in random.sample(poss, k=2):  # cand incepi jocul, ai valori pe 2 pozitii (acel k)
-    if random.random() < .1:
-        grid[pos] = 4  # valoarea poate sa fie 4
-    else:
-        grid[pos] = 2  # dar poate sa fie si 2 (cea mai mare probabilitate)
+    def __str__(self):  # vrem sa pritam doar grila momentan...
+        return str(self.grid)
 
-print(grid)
+    def new_number(self, k=1):
+        free_poss = list((zip(*np.where(self.grid == 0))))  # zip le grupeaza 2 cate 2, adica inainte lista ta era [(0, 0, 0, 0, 1, 1, etc...)] acum este [(0, 0), (0, 1), (0, 2), (0, 3), (1, 0), etc...]
+
+        for pos in random.sample(free_poss, k=k):  # cand incepi jocul, ai valori pe 2 pozitii (acel k)
+            if random.random() < .1:  # aici cred ca a pus .1 pentru ca probablitatea sa fie 4 este mai mica de 10 %
+                self.grid[pos] = 4  # valoarea poate sa fie 4
+            else:
+                self.grid[pos] = 2  # dar poate sa fie si 2 (cea mai mare probabilitate)
+
+
+if __name__ == '__main__':  # initializam jocul
+    game = Py2048Logic()
+    print(game)
